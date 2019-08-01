f2e5710d53eacac8953eb309661e0e92ae8f08fa
carsin <carsonfreedman1@gmail.com>
2019-08-01 12:13:24 -1000

    move tileinfo display based on click position

--- js/site.js
+++ js/site.js
@@ -8,6 +8,7 @@ const marketView = $("#marketView");
 $(document).ready(() => {
     inventoryView.hide();
     marketView.hide();
+    $("#tileInfo").hide();
 });
 
 //
@@ -50,16 +51,19 @@ $(".navButton").click((e) => {
 
 function updateTileInfo() {
     $("td").click((e) => {
+        $("#tileInfo").show();
+        $("#tileInfo").css("top", e.clientY);
+        $("#tileInfo").css("left", e.clientX);
         var tileId = $(e.target).attr("id")
         var currX = tileId.substring(0, tileId.indexOf("_"));
         var currY = tileId.substring(tileId.indexOf("_") + 1);
-
 
         $("#currentTileDisplay").html(gameWorld.map[currX][currY].name);
         $("#tileDescriptionDisplay").html(gameWorld.map[currX][currY].description);
     });
 
     $("td").focusout((e) => {
+        $("#tileInfo").hide();
         $("#currentTileDisplay").html("");
         $("#tileDescriptionDisplay").html("");
     });
